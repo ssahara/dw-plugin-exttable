33bc172844ebf188594436777ddad4a843803321
LarsDW223 <lars_paulsen@web.de>
2015-05-30 11:54:23 +0200

    Added support for ODT export.

--- syntax.php
+++ syntax.php
@@ -279,6 +279,7 @@ class syntax_plugin_exttab3 extends DokuWiki_Syntax_Plugin {
 
         switch ($format) {
             case 'xhtml' : return $this->render_xhtml($renderer, $data);
+            case 'odt'   : return $this->render_odt($renderer, $data);
             default:
                 return true;
         }
@@ -299,6 +300,96 @@ class syntax_plugin_exttab3 extends DokuWiki_Syntax_Plugin {
                 break;
             case DOKU_LEXER_EXIT:     // close tag
                 $renderer->doc.= $this->_close($tag);
+                break;
+        }
+    }
+
+    protected function render_odt(&$renderer, $data) {
+        $properties = array ();
+
+        // Return if installed ODT plugin version is too old.
+        if ( method_exists ($renderer, 'getODTProperties') === false ||
+             method_exists ($renderer, '_odtTableAddColumnUseProperties') === false ) {
+            return;
+        }
+
+        //list($tag, $state, $match) = $data;
+        list($state, $tag, $attr) = $data;
+
+        // Get style content
+        preg_match ('/style=".*"/', $attr, $matches);
+        $style = "";
+        if ( empty ($matches [0]) === false ) {
+            $style = substr ($matches [0], 6);
+            $style = trim ($style, ' "');
+        }
+
+        switch ( $state ) {
+            case DOKU_LEXER_ENTER:    // open tag
+                switch ($tag) {
+                    case 'table':
+                        // Get CSS properties for ODT export.
+                        $renderer->getODTProperties ($properties, 'table', 'dokuwiki exttab3', $style);
+
+                        $renderer->_odtTableOpenUseProperties($properties);
+                        break;
+                    case 'caption':
+                        // Get CSS properties for ODT export.
+                        $renderer->getODTProperties ($properties, 'caption', 'dokuwiki exttab3', $style);
+
+                        $renderer->_odtTableRowOpenUseProperties($properties);
+
+                        // Parameter 'colspan=0' indicates spann across all columns!
+                        $renderer->_odtTableHeaderOpenUseProperties($properties, 0, 1);
+                        break;
+                    case 'th':
+                        // Get CSS properties for ODT export.
+                        $renderer->getODTProperties ($properties, 'th', 'dokuwiki exttab3', $style);
+
+                        $renderer->_odtTableHeaderOpenUseProperties($properties);
+                        $renderer->_odtTableAddColumnUseProperties($properties);
+                        break;
+                    case 'tr':
+                        // Get CSS properties for ODT export.
+                        $renderer->getODTProperties ($properties, 'tr', 'dokuwiki exttab3', $style);
+                        
+                        $renderer->_odtTableRowOpenUseProperties($properties);
+                        break;
+                    case 'td':
+                        // Get CSS properties for ODT export.
+                        $renderer->getODTProperties ($properties, 'td', 'dokuwiki exttab3', $style);
+
+                        $renderer->_odtTableCellOpenUseProperties($properties);
+                        break;
+                }
+                //$renderer->doc.= $this->_open($tag, $attr);
+                break;
+            case DOKU_LEXER_MATCHED:  // defensive, shouldn't occur
+            case DOKU_LEXER_UNMATCHED:
+                $renderer->cdata($tag);
+                break;
+            case DOKU_LEXER_EXIT:     // close tag
+                //$renderer->doc.= $this->_close($tag);
+                switch ($tag) {
+                    case 'table':
+                            //$renderer->table_close();
+                            $renderer->_odtTableClose();
+                        break;
+                    case 'caption':
+                        $renderer->tableheader_close();
+                        $renderer->tablerow_close();
+                        break;
+                    case 'th':
+                        $renderer->tableheader_close();
+                        break;
+                    case 'tr':
+                        $renderer->tablerow_close();
+                        break;
+                    case 'td':
+                        $renderer->p_close();
+                        $renderer->tablecell_close();
+                        break;
+                }
                 break;
         }
     }
